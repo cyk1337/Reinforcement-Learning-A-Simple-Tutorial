9e0ae8388627270967cf6a33e1b5242fee99daa2
cyk1337 <chaiyekun@yahoo.com>
2019-06-11 11:06:38 +0800

    update

--- MovanZhou RL Practice/5 Actor Critic/AC_CartPole.py
+++ MovanZhou RL Practice/5 Actor Critic/AC_CartPole.py
@@ -12,7 +12,7 @@ _____.___._______________  __.____ __________    _________   ___ ___    _____  .
  
 
 @author: Yekun Chai
-@license: Center on Research of Intelligent Systems and Engineering, IA, CAS
+@license: IACAS
 @contact: chaiyekun@gmail.com
 @file: AC_CartPole.py
 @time: 3/27/19 5:06 PM
